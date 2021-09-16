41b8f4255d610cf8deca37d5b9d8292167c13ea3
Alwin Schuster <alwin.schuster@chello.at>
2021-09-16 19:45:15 +0200

    Restructure

--- fakexrandr_background/main.py
+++ fakexrandr_background/main.py
@@ -1,16 +1,12 @@
-import os
-import random
-import tempfile
 import threading
 import time
 import logging
 import argparse
 from pathlib import Path
 
-import cv2
-import numpy as np
-
+from fakexrandr_background.gsetting_actions import update_picture
 from fakexrandr_background.icon import show_icon
+from fakexrandr_background.picture_utils import create_picture, get_paths
 from screen import get_screen
 
 parser = argparse.ArgumentParser(description='fakexrandr-background')
@@ -22,72 +18,6 @@ parser.add_argument('--delay', type=float, default=0, help='A delay at the begin
 
 args = parser.parse_args()
 logging.basicConfig(level=logging.INFO)
-
-
-# From https://stackoverflow.com/a/58034691/6760875
-def crop_and_resize(img, w, h):
-    im_h, im_w, channels = img.shape
-    res_aspect_ratio = w / h
-    input_aspect_ratio = im_w / im_h
-
-    if input_aspect_ratio > res_aspect_ratio:
-        im_w_r = int(input_aspect_ratio * h)
-        im_h_r = h
-        img = cv2.resize(img, (im_w_r, im_h_r))
-        x1 = int((im_w_r - w) / 2)
-        x2 = x1 + w
-        img = img[:, x1:x2, :]
-    if input_aspect_ratio < res_aspect_ratio:
-        im_w_r = w
-        im_h_r = int(w / input_aspect_ratio)
-        img = cv2.resize(img, (im_w_r, im_h_r))
-        y1 = int((im_h_r - h) / 2)
-        y2 = y1 + h
-        img = img[y1:y2, :, :]
-    if input_aspect_ratio == res_aspect_ratio:
-        img = cv2.resize(img, (w, h))
-
-    return img
-
-
-def create_picture(screen, picture_paths):
-    background = np.zeros((screen.height, screen.width, 3), np.uint8)
-    chosen_paths = random.sample(picture_paths, len(screen.displays))
-
-    for i, display in enumerate(screen.displays):
-        img = cv2.imread(str(chosen_paths[i]))
-        img = crop_and_resize(img, display.width, display.height)
-        background[display.top:display.top + display.height, display.left:display.left + display.width] = img
-
-    return background
-
-
-def get_paths(picture_path):
-    pictures = os.listdir(picture_path)
-    pictures = map(lambda path: picture_path / Path(path), pictures)
-    pictures = filter(lambda path: path.is_file(), pictures)
-    pictures = filter(lambda path:
-                      path.name.endswith(".png") or
-                      path.name.endswith(".jpg") or
-                      path.name.endswith(".jpeg"), pictures)
-    return list(pictures)
-
-
-def set_spanned():
-    logging.info("Set gsettings set org.gnome.desktop.background picture-options to spanned")
-    os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-options 'spanned'")
-
-
-def update_picture(screen, picture_paths):
-    logging.info("Generating picture...")
-    background = create_picture(screen, picture_paths)
-
-    with tempfile.NamedTemporaryFile() as tmp:
-        cv2.imwrite(tmp.name + ".jpg", background)
-        logging.info("Picture saved")
-
-        os.system(f"/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///{tmp.name}.jpg")
-        logging.info("Background updated")
 
 
 def loop(screen, picture_paths):
